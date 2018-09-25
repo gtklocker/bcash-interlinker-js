a8145fcd5b5c55307802dd1a1985345560799c89
Kostis Karantias <kkarantias@gmail.com>
2018-09-25 14:23:29 +0000

    txs wip

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const pEvent = require('p-event');
 const LiteNode = require('./LiteNode');
-const { util } = require('bcash');
+const bcash = require('bcash').set('testnet');
+const { fromRev } = require('bcash/lib/utils/util');
 const Interlink = require('./Interlink');
+const taggedSPVTx = require('./script');
 
 const MARKED_BLOCK_HASH =
-  util.fromRev('00000000000001934669a81ecfaa64735597751ac5ca78c4d8f345f11c2237cf');
+  fromRev('00000000000001934669a81ecfaa64735597751ac5ca78c4d8f345f11c2237cf');
 
 async function onTip(node) {
   const interlink = new Interlink();
@@ -24,19 +26,42 @@ async function onTip(node) {
   console.log('interlink hash =', interlink.hash());
 }
 
+const path = require('path');
+const WALLET_LOCATION = path.join(process.env.HOME, '.bcash', 'testnet', 'wallet');
+
 (async () => {
   const node = new LiteNode({chainLocation: './bcash-bak/spvchain/'});
+  const walletdb = new bcash.wallet.WalletDB({location: WALLET_LOCATION, spv: true});
   await node.open();
   await node.connect();
   node.startSync();
+
+  await walletdb.open();
+  console.log(walletdb.options.location);
+  console.log(walletdb.options.spv);
+  console.log('wallets =', await walletdb.getWallets());
+  const wallet = await walletdb.get('primary');
+  const account = await wallet.getAccount('default');
+  const accounts = await wallet.getAccounts();
+  console.log('accounts =', accounts);
+  console.log('receive address =', account.receiveAddress());
 
   if (!node.chain.synced) {
     console.log('not synced, waiting for event...');
     await pEvent(node.chain, 'full');
   }
 
+  wallet.once('balance', async (balance) => {
+    console.log('got balance');
+    const tx = taggedSPVTx(Buffer.from('lol js'));
+    const fundedTx = await wallet.fund(tx);
+    console.log({fundedTx});
+  });
+    /*
+
   await onTip(node);
   node.chain.on('tip', () => onTip(node));
+  */
 })().catch((err) => {
   console.error(err.stack);
   process.exit(1);
