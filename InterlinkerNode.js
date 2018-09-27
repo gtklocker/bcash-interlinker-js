9800d10385cbf7d0b20cc77fb651fc3471f1ded5
Kostis Karantias <kkarantias@gmail.com>
2018-09-27 14:03:08 +0000

    move functionality inside InterlinkerNode

--- InterlinkerNode.js
+++ InterlinkerNode.js
@@ -1,8 +1,70 @@
 const bcash = require('bcash');
+const {fromRev} = require('bcash/lib/utils/util');
+const pEvent = require('p-event');
+
+const Interlink = require('./Interlink');
+const taggedSPVTx = require('./script');
+
+const MARKED_BLOCK_HASH =
+  fromRev('00000000000001934669a81ecfaa64735597751ac5ca78c4d8f345f11c2237cf');
 
 module.exports = class InterlinkerNode extends bcash.SPVNode {
   constructor(options) {
     super(options);
     this.walletDB = this.use(bcash.wallet.plugin).wdb;
+    this.ourLogger = this.logger.context('interlinker');
+  }
+
+  async startInterlinking() {
+    if (!this.chain.synced) {
+      this.ourLogger.info('not synced, waiting for event...');
+      await pEvent(node.chain, 'full');
+    }
+    this.ourLogger.info('starting interlinking');
+    this.doInterlink();
+    this.on('tip', this.doInterlink);
+  }
+
+  // TODO: This should not be needed, but we are currently working around
+  // https://github.com/bcoin-org/bcash/issues/95
+  async sendMTX(mtx) {
+    const wallet = this.walletDB.primary;
+    try {
+      await wallet.fund(mtx);
+    } catch (e) {
+      this.ourLogger.error('not enough funds to publish interlink tx');
+      this.ourLogger.error('feed me:', await wallet.receiveAddress());
+      return; // TODO: replace with InsufficientFundsError
+    }
+    await wallet.sign(mtx);
+    let tx = mtx.toTX();
+    await this.walletDB.addTX(tx);
+    await this.walletDB.send(tx);
+    this.ourLogger.info('sent tx', tx.hash().toString('hex'));
+    return tx;
+  }
+
+  async doInterlink() {
+    const interlink = await this.getInterlinkSinceBlock(MARKED_BLOCK_HASH);
+    const hash = interlink.hash();
+
+    this.ourLogger.info('interlink hash =', hash.toString('hex'));
+    await this.sendMTX(taggedSPVTx(hash));
+  }
+
+  async getInterlinkSinceBlock(blockId) {
+    const interlink = new Interlink();
+    let blk = await this.chain.getEntryByHash(blockId);
+    let blockCount = 0;
+    let lastBlock = blk;
+    while (blk) {
+      ++blockCount;
+      lastBlock = blk;
+      interlink.update(blk.hash);
+      blk = await this.chain.getNextEntry(blk);
+    }
+    this.ourLogger.info('saw', blockCount, 'blocks');
+    this.ourLogger.info('last block =', lastBlock.hash.toString('hex'));
+    return interlink;
   }
 };
--- index.js
+++ index.js
@@ -1,76 +1,16 @@
 'use strict';
 
-const pEvent = require('p-event');
 const InterlinkerNode = require('./InterlinkerNode');
-const {fromRev} = require('bcash/lib/utils/util');
-const Interlink = require('./Interlink');
-const taggedSPVTx = require('./script');
-
-const MARKED_BLOCK_HASH =
-  fromRev('00000000000001934669a81ecfaa64735597751ac5ca78c4d8f345f11c2237cf');
-
-async function onTip(node, wallet) {
-  const interlink = new Interlink();
-  let blk = await node.chain.getEntryByHash(MARKED_BLOCK_HASH);
-  let blockCount = 0;
-  let lastBlock = blk;
-  while (blk) {
-    ++blockCount;
-    lastBlock = blk;
-    interlink.update(blk.hash);
-    blk = await node.chain.getNextEntry(blk);
-  }
-  console.log('saw', blockCount, 'blocks');
-  console.log('last block =', lastBlock);
-  console.log('interlink hash =', interlink.hash());
-
-  const tx = taggedSPVTx(interlink.hash());
-  try {
-    await wallet.fund(tx);
-  } catch (e) {
-    console.error('not enough funds to publish interlink tx');
-    console.error('feed me:', await wallet.receiveAddress());
-    return;
-  }
-  await wallet.sign(tx);
-  const finalTx = tx.toTX();
-  console.log('final tx =', finalTx);
-  try {
-    await node.walletDB.addTX(finalTx);
-    await node.walletDB.send(finalTx);
-  } catch (e) { console.error('error:', e); }
-}
 
 (async () => {
-  const node = new InterlinkerNode({memory: false, env: true, logLevel: 'info'});
+  const node = new InterlinkerNode({memory: false, env: true, workers: true, logLevel: 'info'});
   await node.ensure();
   console.log('prefix =', node.config.prefix);
   console.log('network =', node.chain.network);
   await node.open();
   await node.connect();
   node.startSync();
-
-  const wallet = node.walletDB.primary;
-
-  setInterval(() => {
-    console.log('chain height =', node.chain.height);
-  }, 5000);
-
-
-  if (!node.chain.synced) {
-    console.log('not synced, waiting for event...');
-    await pEvent(node.chain, 'full');
-  }
-
-  console.log('cool');
-
-  console.log('* balance = ', await wallet.getBalance());
-  wallet.on('balance', async (balance) => {
-    console.log('! balance = ', balance);
-  });
-
-  await onTip(node, wallet);
-  node.chain.on('tip', () => onTip(node, wallet));
+  node.startInterlinking();
 })().catch((err) => {
   console.error(err.stack);
   process.exit(1);
